5b8c504f7db82ffca06c639da83d3ac9dc793009
Manuel <manlara@gmail.com>
2020-11-23 20:37:40 -0600

    fixed map style

--- static/js/map.js
+++ static/js/map.js
@@ -6,7 +6,7 @@ var myMap = L.map("map", {
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/satellite-streets-v11',
+    id: 'mapbox/dark-v10',
     accessToken: API_KEY
 }).addTo(myMap);
 
